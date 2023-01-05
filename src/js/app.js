933a79e7e50c8fe994b9398a67da6ba9754cdbdb
Juan Pablo Yagüe <juanpabloyague@gmail.com>
2023-01-05 11:27:49 -0500

    refactor validation

--- src/js/app.js
+++ src/js/app.js
@@ -1,71 +1,58 @@
 let customer = {
+  name: "",
+  people: "",
   table: "",
   time: "",
-  order: [],
 };
 
-// Event Listeners
-const name = document.querySelector("#name");
-const email = document.querySelector("#email");
-const table = document.querySelector("#table");
-const time = document.querySelector("#time");
-
-const formContact = document.querySelector(".form");
-const btnSaveCustomer = document.querySelector("#save-customer");
-btnSaveCustomer.addEventListener("click", saveCustomer);
-
-// functions
-
-// check if email is valid
-function isValidEmail(email) {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-}
-
-function checkRequired(inputArr) {
-  inputArr.forEach((input) => {
-    const { value, id } = input;
-    if (value.trim() === "") {
-      showError(input, `${id} is required`);
-    } else {
-      input.classList.remove("error");
-    }
-  });
-}
-
-// Check length
-function checkLength(input, min, max) {
-  const { value, id } = input;
-  if (value.length < min) {
-    showError(input, `${id} must be at least ${min} characters `);
-  } else if (value.length > max) {
-    showError(input, `${id} must be less than ${max} characters`);
-  } else {
-    input.classList.remove("error");
-  }
-}
+const formContainer = document.querySelector(".form");
+formContainer.addEventListener("submit", saveCustomer);
 
 function saveCustomer(e) {
   e.preventDefault();
+  const name = document.querySelector("#name").value;
+  const people = document.querySelector("#people").value;
+  const table = document.querySelector("#table").value;
+  const time = document.querySelector("#time").value;
 
-  checkRequired([name, email]);
-  checkLength(name, 3, 15);
+  // Checko for empy fields
+  const emptyFields = [name, people, table, time].some((field) => field === "");
+
+  if (emptyFields) {
+    // Check if there is already an alert
+    const existAlert = document.querySelector(".alert-box");
+
+    if (existAlert) return;
+
+    const alert = document.createElement("DIV");
+    alert.classList.add("alert-box");
+    setTimeout(() => {
+      alert.classList.add("error");
+    }, 200);
+
+    const alertMessage = document.createElement("P");
+    alertMessage.classList.add("alert-text");
+    alertMessage.textContent = "All fields are required";
+
+    alert.appendChild(alertMessage);
+
+    document.querySelector(".form").appendChild(alert);
+
+    setTimeout(() => {
+      alert.remove();
+    }, 3000);
+    return;
+  }
+
+  // Assign data from the form to customer
+  customer = { ...customer, name, people, table, time };
+
+  // hide form
+  const formContainer = document.querySelector(".content-contact");
+  formContainer.classList.add("hidden");
+
+  // Show sections
+  showSections();
 }
 
-function showSuccess() {
-  // const errorBox = document.createElement("DIV");
-  // const errorText = document.createElement("P");
-  // errorText.classList.add("alert-text");
-  // errorBox.classList.add("error-box");
-}
-
-function showError(input, msg) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector("small");
-
-  input.classList.add("error");
-  small.innerText = msg;
-}
+function showSections() {}
